cac409d82e9153331b913ec1f6099ae952b7fc3e
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-09-25 22:32:40 -0500

    Refactor of code

--- app/src/main/java/com/example/kotlinfundamentals/classes/Classes.kt
+++ app/src/main/java/com/example/kotlinfundamentals/classes/Classes.kt
@@ -16,13 +16,29 @@ fun main(){
     println("Privado? ${objSmartPhone.esPrivado}")
 
     renewTopic("Data classes")
-    val objUser:User = User(0,"Michael","Acosta",0)
-    println(objUser)
-    println(objUser.group)
-    println(objUser.name)
-    println(objUser.lastName)
-    println(objUser.group)
-    println(objUser.component1())
+    val objUser1:User = User(0,"Michael","Acosta",Group.FRIEND.ordinal)
+    val objUser2:User = objUser1.copy(1,"Camila")
+    val objUser3:User = objUser2.copy(2, group = Group.FAMILY.ordinal)
+    println(objUser1)
+    println(objUser2)
+    println(objUser3)
 
+    println(objUser1.group)
+    println(objUser1.name)
+    println(objUser1.lastName)
+    println(objUser1.group)
+    println(objUser1.component1())
 
+    renewTopic("Funciones de alcance")
+    with(objSmartPhone){
+        println("Es privado? $esPrivado")
+        println(showNumber())
+        call()
+    }
+    objUser3.apply{
+        group = Group.FRIEND.ordinal
+        name="Jose"
+        lastName="Perez"
+    }
+    println(objUser3)
 }
--- app/src/main/java/com/example/kotlinfundamentals/classes/SmartPhone.kt
+++ app/src/main/java/com/example/kotlinfundamentals/classes/SmartPhone.kt
@@ -3,7 +3,7 @@ package com.example.kotlinfundamentals.classes
 class SmartPhone(numero:Int,val esPrivado:Boolean):Phone(numero){
     override fun showNumber() {
         if(esPrivado){
-            println("Desconocido")}
+            println("Desconocido por que es privado")}
         else super.showNumber()
     }
 }
