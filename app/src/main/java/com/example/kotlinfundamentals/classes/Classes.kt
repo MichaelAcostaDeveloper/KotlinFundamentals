1fcc465901424bb9cb75a6e126107b376827725b
MichaelAcostaDeveloper <acostamichael80desarrollo@gmail.com>
2022-09-25 21:39:53 -0500

    Refactor of code

--- app/src/main/java/com/example/kotlinfundamentals/classes/Classes.kt
+++ app/src/main/java/com/example/kotlinfundamentals/classes/Classes.kt
@@ -8,10 +8,21 @@ fun main(){
     objPhone.call()
     objPhone.showNumber()
     //println(objPhone.number)
-    renewTopic("Herencia")
 
+    renewTopic("Herencia")
     val objSmartPhone:SmartPhone = SmartPhone(1193,true)
     objSmartPhone.call()
     objSmartPhone.showNumber();
     println("Privado? ${objSmartPhone.esPrivado}")
+
+    renewTopic("Data classes")
+    val objUser:User = User(0,"Michael","Acosta",0)
+    println(objUser)
+    println(objUser.group)
+    println(objUser.name)
+    println(objUser.lastName)
+    println(objUser.group)
+    println(objUser.component1())
+
+
 }
